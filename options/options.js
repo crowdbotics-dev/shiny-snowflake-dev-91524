3360cb3bbb80c964dc66a19d5ec5e2d9ee391b18
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 13:14:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-snowflake-dev-91524",
+  url: "https://shiny-snowflake-dev-91524.botics.co",
+  api: "https://shiny-snowflake-dev-91524.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
